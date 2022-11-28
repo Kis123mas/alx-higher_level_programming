bc43df2ad4739aa1bdf9342ac99ba99c6f083de9
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-11-28 15:11:48 +0100

    all files were worked on locally by Emmanuel Solomon.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
 const a = args.length;
-if (a === 0) {
+if (a === 2) {
   console.log('No argument');
-} else if (a === 1) {
+} else if (a === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
